45620837e2c164f0353f144c16f01ca6f5424907
KeithRussel <keithmaniacop@gmail.com>
2022-04-09 19:55:48 +0800

    Connect Mongo Database to backend server

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,35 +1,37 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc    Get posts
 // @route   GET /api/posts
 // @access  Public
-const getPosts = (req, res) => {
+const getPosts = asyncHandler(async (req, res) => {
   res.json({ message: "Get posts" });
-};
+});
 
 // @desc    Set post
 // @route   POST /api/posts
 // @access  Private
-const setPosts = (req, res) => {
+const setPosts = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please add a text field");
   }
 
   res.json({ message: "Set post" });
-};
+});
 
 // @desc    Update post
 // @route   GET /api/posts/:id
 // @access  Private
-const updatePost = (req, res) => {
+const updatePost = asyncHandler(async (req, res) => {
   res.json({ message: `Update post ${req.params.id}` });
-};
+});
 
 // @desc    Delete post
 // @route   DELETE /api/posts
 // @access  Private
-const deletePost = (req, res) => {
+const deletePost = asyncHandler(async (req, res) => {
   res.json({ message: `Delete post ${req.params.id}` });
-};
+});
 
 module.exports = {
   getPosts,
