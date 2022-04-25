256ca6022a1794a6e741d242f5d7405a9e8bf507
KeithRussel <keithmaniacop@gmail.com>
2022-04-25 16:36:38 +0800

    Edit post button hide if user id not equal to post user id

--- frontend/src/components/PostItem.jsx
+++ frontend/src/components/PostItem.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import {FaEllipsisH, FaEdit, FaTrash} from 'react-icons/fa'
-import { useDispatch} from 'react-redux'
+import { useDispatch, useSelector} from 'react-redux'
 import {deletePost, getPost, nullPost} from '../features/posts/postSlice'
 import ModalSet from '../components/ModalSet'
 import {Link, useParams} from 'react-router-dom'
 
 function PostItem({post}) {
     const [showButton, setShowButton] = useState(false)
+    const {user} = useSelector((state)=>state.auth)
+
     const onClick = () => {
         setShowButton(!showButton)
         console.log(postId)
@@ -31,33 +33,33 @@ function PostItem({post}) {
     return (
         <>
         {isModalOpen && <ModalSet post={post} onRequestClose={toggleModal} />}
-    <div className="posts">
-        <div className='posts__head'>
-            <div className="posts__head_flex">
-            <div className="user__avatar"></div>
-            <div className="posts__head_flex_col">
-                <div className="user__name">{post.user.name}</div>
-                <div className="posted__time"><small>{new Date(post.createdAt).toLocaleString('en-US')}</small></div>
+            <div className="posts">
+                <div className='posts__head'>
+                    <div className="posts__head_flex">
+                    <div className="user__avatar"></div>
+                    <div className="posts__head_flex_col">
+                        <div className="user__name">{post.user.name}</div>
+                        <div className="posted__time"><small>{new Date(post.createdAt).toLocaleString('en-US')}</small></div>
+                    </div>
+                    </div>
+                    {post.user._id === user._id ? <div className='posts__head_ellipsis'>
+                        <div onClick={onClick}><FaEllipsisH /></div>
+                        {showButton ?<div>
+                            <button type="button"><Link key={post._id} to={`/edit/${post._id}`}><FaEdit /> Edit</Link></button>
+                            <button onClick={() => dispatch(deletePost(post._id))}><FaTrash /> Delete</button>
+                        </div> : null }
+                    </div> : null }
+                    
+                </div>
+                <div className="user__post">
+                    <p>{post.text}</p>
+                </div>
+                <div className="posts__buttons btn-group">
+                    <div type="button" className="likes">{post.likes.length < 1 ? <span>Like</span> : <span>{post.likes.length} Likes</span>  }</div>
+                    <div type="button" className="comments">Comment</div>
+                </div>
             </div>
-            </div>
-            <div className='posts__head_ellipsis'>
-                <div onClick={onClick}><FaEllipsisH /></div>
-                {showButton ?<div>
-                    <button type="button"><Link key={post._id} to={`/edit/${post._id}`}><FaEdit /> Edit</Link></button>
-                    <button onClick={() => dispatch(deletePost(post._id))}><FaTrash /> Delete</button>
-                </div> : null }
-                
-            </div>
-        </div>
-        <div className="user__post">
-            <p>{post.text}</p>
-        </div>
-        <div className="posts__buttons btn-group">
-            <div type="button" className="likes">{post.likes.length < 1 ? <span>Like</span> : <span>{post.likes.length} Likes</span>  }</div>
-            <div type="button" className="comments">Comment</div>
-        </div>
-    </div>
-    </>
+        </>
     )
 }
 
